bebcc05e93eb0a1534babfd0d6d714dc725a7e73
root <root@ip-10-224-64-190.us-west-2.compute.internal>
2019-07-17 23:20:33 +0000

    headers3

--- src/secured/page1.php
+++ src/secured/page1.php
@@ -25,6 +25,12 @@
                         fclose($myfile);
 
                         print_r($_SERVER)
+
+    $headers = apache_request_headers();
+
+    foreach ($headers as $header => $value) {
+        echo "$header: $value <br />\n";
+    }
                 ?>
 
             </div>
